00f4a9f131d5191a6427c4e85e05851d3c81913d
Víctor Merino <victor.merino@jungheinrich.es>
2022-12-22 20:32:02 +0100

    Fix basic routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { MarvelPage } from '../heroes/pages/MarvelPage'
 import { DCPage } from '../heroes/pages/DCPage'
 import { LoginPage } from '../auth/pages/LoginPage'
@@ -8,10 +8,12 @@ export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="marvel" element={<DCPage />} />
-        <Route path="dc" element={<MarvelPage />} />
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DCPage />} />
 
         <Route path="login" element={<LoginPage />} />
+
+        <Route path="/" element={<Navigate to="/marvel" />} />
       </Routes>
     </>
   )
