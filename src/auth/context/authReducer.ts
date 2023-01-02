ed39ef44a890332503a5bbf5109c6147f43d9e67
Víctor Merino <victor.merino@jungheinrich.es>
2023-01-02 11:22:18 +0100

    Better to set guards at the beggining of the function

--- src/auth/context/authReducer.ts
+++ src/auth/context/authReducer.ts
@@ -1,21 +1,20 @@
 import { AuthAction, Creators } from './types'
 
 export const authReducer = (state = {}, action?: AuthAction | null) => {
-  if (action) {
-    switch (action.type) {
-      case Creators.login:
-        return {
-          logged: true,
-          user: action.payload,
-        }
-      case Creators.logout:
-        return {
-          logged: false,
-          user: null,
-        }
-      default:
-        return state
-    }
+  if (!action) return state
+
+  switch (action.type) {
+    case Creators.login:
+      return {
+        logged: true,
+        user: action.payload,
+      }
+    case Creators.logout:
+      return {
+        logged: false,
+        user: null,
+      }
+    default:
+      return state
   }
-  return state
 }
