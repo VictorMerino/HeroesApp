6b5cf5dd004a396c4883bd3e7bdcfca597f63bae
Víctor Merino <victor.merino@jungheinrich.es>
2023-01-02 11:22:36 +0100

    authReducer tests

---
+++ tests/auth/context/authReducer.test.ts
@@ -0,0 +1,55 @@
+/**
+ * Login should call login, login and set the user
+ * Logout should delete user name and logged to false
+ */
+
+import { authReducer } from '../../../src/auth/context'
+import { Creators } from '../../../src/auth/context/types'
+
+const EMPTY_STATE = {}
+const DEMO_USER = {
+  name: 'Victorius',
+  id: 'AAA',
+}
+const LOGGED_STATE = {
+  user: DEMO_USER,
+  logged: true,
+}
+const UNLOGGED_STATE = {
+  user: null,
+  logged: false,
+}
+describe('authReducer', () => {
+  test('should return default state if no action provided', () => {
+    const INITIAL_STATE = EMPTY_STATE
+    const newState = authReducer(INITIAL_STATE)
+    expect(newState).toBe(INITIAL_STATE)
+  })
+  test('should return default state if null action provided', () => {
+    const INITIAL_STATE = EMPTY_STATE
+    const newState = authReducer(INITIAL_STATE, null)
+    expect(newState).toBe(INITIAL_STATE)
+  })
+  test('should call login and set the user on Login', () => {
+    const INITIAL_STATE = EMPTY_STATE
+    const USER = DEMO_USER
+
+    const AUTH_ACTION = {
+      type: Creators.login,
+      payload: USER,
+    }
+    const EXPECTED_STATE = LOGGED_STATE
+    const newState = authReducer(INITIAL_STATE, AUTH_ACTION)
+    expect(newState).toEqual(EXPECTED_STATE)
+  })
+  test('should delete user name and set logged to false on Logout', () => {
+    // const USER = DEMO_USER
+    const INITIAL_STATE = LOGGED_STATE
+    const AUTH_ACTION = {
+      type: Creators.logout,
+    }
+    const EXPECTED_STATE = UNLOGGED_STATE
+    const newState = authReducer(INITIAL_STATE, AUTH_ACTION)
+    expect(newState).toEqual(EXPECTED_STATE)
+  })
+})
