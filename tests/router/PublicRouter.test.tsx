e9e1a5d0c5d178d744087b6ed3235175dd9a20df
Víctor Merino <victor.merino@jungheinrich.es>
2023-01-02 16:26:07 +0100

    Logged or not, public routes should always return children

--- tests/router/PublicRouter.test.tsx
+++ tests/router/PublicRouter.test.tsx
@@ -1,15 +1,30 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { MemoryRouter } from 'react-router-dom'
 import { AuthContext } from '../../src/auth'
 import { PublicRouter } from '../../src/router'
-import { UNLOGGED_STATE } from '../helpers/auth'
+import { LOGGED_STATE, UNLOGGED_STATE } from '../helpers/auth'
 
-describe.skip('<PublicRouter>', () => {
+const renderPublicRoute = (state = {}) => {
+  return (
+    <AuthContext.Provider value={state}>
+      <MemoryRouter>
+        <PublicRouter>
+          <h1>Random public route</h1>
+        </PublicRouter>
+      </MemoryRouter>
+    </AuthContext.Provider>
+  )
+}
+describe('<PublicRouter>', () => {
   test('should return children if not logged', () => {
-    render(
-      <AuthContext.Provider value={UNLOGGED_STATE}>
-        <PublicRouter />
-      </AuthContext.Provider>
-    )
+    render(renderPublicRoute(UNLOGGED_STATE))
+
+    expect(screen.getByText('Random public route'))
+  })
+  test('should return children if logged', () => {
+    render(renderPublicRoute(LOGGED_STATE))
+
+    expect(screen.getByText('Random public route'))
   })
 })
