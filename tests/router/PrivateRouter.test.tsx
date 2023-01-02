34b29fb8225700436b8e58dc2dae1d701a655580
Víctor Merino <victor.merino@jungheinrich.es>
2023-01-02 15:38:13 +0100

    Tests for PrivateRouter: pass if logged, reject to HomePage if not

---
+++ tests/router/PrivateRouter.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
+import { AuthContext } from '../../src/auth'
+import { PrivateRouter } from '../../src/router'
+import { LOGGED_STATE, UNLOGGED_STATE } from '../helpers/auth'
+
+describe('<PrivateRouter>', () => {
+  test('should return children if logged', () => {
+    render(
+      <AuthContext.Provider value={LOGGED_STATE}>
+        <PrivateRouter />
+      </AuthContext.Provider>
+    )
+  })
+
+  test('should reject if unlogged', () => {
+    // Skipped because it's looping eternally
+    render(
+      <AuthContext.Provider value={UNLOGGED_STATE}>
+        <MemoryRouter initialEntries={['/admin']}>
+          <Routes>
+            <Route
+              path="admin"
+              element={
+                <PrivateRouter>
+                  <h1>Admin page</h1>
+                </PrivateRouter>
+              }
+            />
+            <Route path="/" element={<h1>Home page</h1>} />
+          </Routes>
+        </MemoryRouter>
+      </AuthContext.Provider>
+    )
+
+    screen.debug()
+    expect(screen.getByText('Home page')).toBeTruthy()
+  })
+})
